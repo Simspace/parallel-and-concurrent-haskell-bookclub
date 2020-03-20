4ed8602d14598dfc7aef59c23d7ab1415a0f974b
shawn-bachlet <shawn.bachlet@simspace.com>
2020-03-20 13:10:26 -0400

    Initial implemenation

--- Bound.hs
+++ Bound.hs
@@ -2,9 +2,38 @@
 {-# LANGUAGE RecordWildCards #-}
 module Bound where
 
-import Control.Concurrent
-import Control.Concurrent.Chan
+import Control.Concurrent hiding (Chan(..))
+-- import Control.Concurrent.Chan
 import Data.Vector
+
+type Stream a = MVar (Item a)
+data Item a = Item a (Stream a)
+
+data Chan a = Chan
+  { readVar  :: (MVar (Stream a))
+  , writeVar :: (MVar (Stream a))
+  }
+
+newChan :: IO (Chan a)
+newChan = do
+  hole <- newEmptyMVar
+  readVar <- newMVar hole
+  writeVar <- newMVar hole
+  return (Chan readVar writeVar)
+
+readChan :: Chan a -> IO a
+readChan Chan{..} = do
+  stream <- takeMVar readVar
+  Item val tail <- takeMVar stream
+  putMVar readVar tail
+  return val
+
+writeChan :: Chan a -> a -> IO ()
+writeChan Chan{..} val = do
+  newHole <- newEmptyMVar
+  oldHole <- takeMVar writeVar
+  putMVar oldHole (Item val newHole)
+  putMVar writeVar newHole
 
 data BoundedChan a = BoundedChan
   { capacity :: Int
@@ -18,10 +47,13 @@ newBoundedChan n = BoundedChan n <$> newMVar 0 <*> newMVar 0 <*> replicateM n ne
 
 writeBoundedChan :: BoundedChan a -> a -> IO ()
 writeBoundedChan BoundedChan{..} a = do
-  r <- takeMVar readPos
-  w <- takeMVar writePos
-  modifyMVar writePos \w -> 
-
+  i <- takeMVar writePos
+  putMVar (contents ! i) a
+  putMVar writePos $ (i + 1) `mod` capacity
 
 readBoundedChan :: BoundedChan a -> IO a
-readBoundedChan BoundedChan{..} = _
+readBoundedChan BoundedChan{..} = do
+  i <- takeMVar readPos
+  x <- takeMVar $ contents ! i
+  putMVar readPos $ (i + 1) `mod` capacity
+  pure x
