af617c71a43eab8de5e11e980b05309ba526f55e
Stuart Terrett <shterrett@gmail.com>
2020-03-26 08:36:58 -0400

    Exception safe bounded mvar queue (I think)

--- src/Bound.hs
+++ src/Bound.hs
@@ -22,20 +22,19 @@ newBoundedChan n = BoundedChan n <$> newMVar 0 <*> newMVar 0 <*> V.replicateM n 
 -- mvar in that index. If that MVar is full, then write will block. A full
 -- MVar indicates that the index has not yet been read.
 writeBoundedChan :: BoundedChan a -> a -> IO ()
-writeBoundedChan c@BoundedChan{..} a = do
-      w <- takeMVar writePos
+writeBoundedChan c@BoundedChan{..} a =
+    modifyMVar_ writePos $ \w -> do
       putMVar (contents V.! w) a -- blocks until empty
-      putMVar writePos (nextIndex c w)
+      pure $ nextIndex c w
 
 -- | Reading locks the read index, and attempts to take the payload from
 -- the mvar in that index. If that MVar is not full, then read will block.
 -- A full MVar indicates that the index has not yet been written
 readBoundedChan :: BoundedChan a -> IO a
-readBoundedChan c@BoundedChan{..} = do
-    r <- takeMVar readPos
-    a <- takeMVar $ contents V.! r
-    putMVar readPos (nextIndex c r)
-    pure a
+readBoundedChan c@BoundedChan{..} =
+    modifyMVar readPos $ \r -> do
+      a <- takeMVar $ contents V.! r
+      pure (nextIndex c r, a)
 
 -- | Indexes for read and write proceed "around" the circular buffer
 -- the synchronization of the MVars keeps them from overlapping incorrectly
--- test/BoundedQueueSpec.hs
+++ test/BoundedQueueSpec.hs
@@ -1,20 +1,23 @@
 module BoundedQueueSpec where
 
-import Control.Monad (forM_)
+import Control.Exception.Base (AsyncException(..))
+import Control.Monad (forM_, forever)
 import Control.Monad.IO.Class (liftIO)
 import Data.List (partition)
+import System.Timeout (timeout)
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import Test.QuickCheck.Monadic
 import Bound
 import qualified Data.Set as Set
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, throwTo, newMVar, modifyMVar_, takeMVar)
 
 tests :: TestTree
 tests =
     testGroup "Bounded Queue"
       [ testProperty "sent and received are the same" sentAndReceived
       , testProperty "partitioned order" partitionedOrder
+      , testProperty "exception safety" exceptionSafe
       ]
 
 sentAndReceived :: [Int] -> [Int] -> Property
@@ -34,3 +37,24 @@ partitionedOrder xs ys = monadicIO $ do
   let (xs', ys') = partition ((1 ==) . fst) zs
   assert $ (snd <$> xs') == xs
   assert $ (snd <$> ys') == ys
+
+exceptionSafe :: [Int] -> Property
+exceptionSafe xs = monadicIO $ do
+  chan <- liftIO $ newBoundedChan 8
+  -- insert and read forever so they're still doing stuff when
+  -- unceremoniously killed
+  wThread <- liftIO $ forkIO $ forM_ (cycle xs) (writeBoundedChan chan)
+  rThread <- liftIO $ forkIO $ forever readBoundedChan chan
+  -- unceremoniously kill the threds
+  liftIO $ throwTo wThread ThreadKilled
+  liftIO $ throwTo rThread ThreadKilled
+
+  -- write everything, and then read it. Timeout will hit probably after
+  -- the write is exhausted
+  liftIO $ forkIO $ forM_ xs (writeBoundedChan chan)
+  zs <- liftIO $ newMVar []
+  liftIO $ timeout (1 * 10 ^ 6) $ forever $ do
+    z <- readBoundedChan chan
+    modifyMVar_ zs $ \zs' -> pure $ z:zs'
+  zs' <- liftIO $ takeMVar zs
+  assert $ Set.fromList zs' == Set.fromList xs
